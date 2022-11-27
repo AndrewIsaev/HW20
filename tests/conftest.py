51431e04f5c42817c427720086dd31c7f9ee3d90
andrey.isaev <an.isaev7@gmail.com>
2022-11-27 15:39:51 +0300

    add test for genre service

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@ import pytest
 from unittest.mock import MagicMock
 
 from dao.director import DirectorDAO
+from dao.genre import GenreDAO
 from dao.model.director import Director
+from dao.model.genre import Genre
 from setup_db import db
 
 
@@ -24,4 +26,20 @@ def director_dao():
 
     return director_dao
 
+@pytest.fixture()
+def genre_dao():
+    genre_dao = GenreDAO(None)
 
+    genre_1 = Genre(id=1, name="genre1")
+    genre_2 = Genre(id=2, name="genre2")
+    genre_3 = Genre(id=3, name="genre3")
+
+    genres = {1: genre_1, 2: genre_2, 3: genre_3}
+
+    genre_dao.get_all = MagicMock(return_value=genres.values())
+    genre_dao.get_one = MagicMock(return_value=genre_1)
+    genre_dao.create = MagicMock(return_value=Genre(id=4, name="genre4"))
+    genre_dao.delete = MagicMock()
+    genre_dao.update = MagicMock()
+
+    return genre_dao
--- tests/test_services/test_genre_service.py
+++ tests/test_services/test_genre_service.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import MagicMock
 from service.genre import GenreService
 
+
 class TestGenreService():
     @pytest.fixture(autouse=True)
     def genre_service(self, genre_dao):
@@ -10,4 +11,24 @@ class TestGenreService():
     def test_get_all(self):
         genres = self.genre_service.get_all()
         assert genres is not None
-        assert len(genres)>0
+        assert len(genres) > 0
+
+    def test_get_one(self):
+        genre = self.genre_service.get_one(1)
+        assert genre is not None
+        assert genre.id == 1
+        assert genre.name == "genre1"
+
+    def test_create(self):
+        data = {"id": 4, "name": "genre4"}
+        genre = self.genre_service.create(data)
+        assert genre is not None
+        assert genre.id == 4
+        assert genre.name == "genre4"
+
+    def test_update(self):
+        data = {"id": 4, "name": "genre4"}
+        assert self.genre_service.update(data)
+
+    def test_delete(self):
+        assert self.genre_service.delete(10) is None
